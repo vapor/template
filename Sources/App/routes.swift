2eaea1a27571669e041ffe86fc6104403fda69f4
logan <logan.william.wright@gmail.com>
2019-02-05 12:22:24 +0100

    leaf view file render on main address

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,12 +4,16 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        #if(leaf) {
+        return try req.view().render("welcome")
+        } else {
+        return "it works!"
+        }
     }
     
     // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello, world!"
+        return "hello, world!"
     }
 
     #if(leaf) {
