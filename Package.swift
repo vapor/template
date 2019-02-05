4c731d5e415040e00c3009e1c66692cce91e4439
logan <logan.william.wright@gmail.com>
2019-02-05 12:09:18 +0100

    update package.swift dependency leaf

--- Package.swift
+++ Package.swift
@@ -6,17 +6,20 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-
         #if(fluent) {
-            .package(url: "https://github.com/vapor/fluent-#lowercase(fluentdb).git", from: "#(fluentversion)")
+        .package(url: "https://github.com/vapor/fluent-#lowercase(fluentdb).git", from: "#(fluentversion)"),
+        } #if(leaf) {
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         }
     ],
     targets: [
         .target(name: "App", dependencies: [
             "Vapor",
             #if(fluent) {
-            "Fluent#(fluentdb)", }
+            "Fluent#(fluentdb)", } #if(leaf) {
+            "Leaf",
+            }
         ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
