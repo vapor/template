d8ed1484d0458b37ce59380cd3ea267c392bb17d
Logan Wright <logan.william.wright@gmail.com>
2019-09-06 00:08:08 +0200

    latest leaf syntax

--- Package.swift
+++ Package.swift
@@ -6,20 +6,21 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        #if(fluent) {
+        #if(fluent):
         .package(url: "https://github.com/vapor/fluent-#lowercase(fluentdb).git", from: "#(fluentversion)"),
-        } #if(leaf) {
+        #endif 
+        #if(leaf):
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-        }
+        #endif
     ],
     targets: [
         .target(name: "App", dependencies: [
             "Vapor",
-            #if(fluent) {
-            "Fluent#(fluentdb)", } #if(leaf) {
+            #if(fluent):
+            "Fluent#(fluentdb)",#endif #if(leaf):
             "Leaf",
-            }
+            #endif
         ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
