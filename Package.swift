765452bf2a2ccf49b027b2bf09fe20380e49427c
Tanner Nelson <me@tanner.xyz>
2019-10-09 15:10:50 -0400

    improve options

--- Package.swift
+++ Package.swift
@@ -7,10 +7,10 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.2"){{#fluent}},
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-alpha.2"),
-        .package(url: "https://github.com/vapor/fluent-{{fluent.db}}-driver.git", from: "4.0.0-alpha.3"){{/fluent}}
+        .package(url: "https://github.com/vapor/fluent-{{lowercase(fluent.db)}}-driver.git", from: "4.0.0-alpha.3"){{/fluent}}
     ],
     targets: [
-        .target(name: "App", dependencies: [{{#fluent}}"Fluent", "FluentSQLiteDriver", {{/fluent}}"Vapor"]),
+        .target(name: "App", dependencies: [{{#fluent}}"Fluent", "Fluent{{fluent.db}}Driver", {{/fluent}}"Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
