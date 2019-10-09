d8c072202a989f72bd3483607fd64d40a6ffa600
Tanner Nelson <me@tanner.xyz>
2019-10-09 15:20:55 -0400

    fluent db version

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     name: "app",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.2"){{#fluent}},
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-alpha.2"),
-        .package(url: "https://github.com/vapor/fluent-{{fluent.db.url}}-driver.git", from: "4.0.0-alpha.3"){{/fluent}}
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha"){{#fluent}},
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-alpha"),
+        .package(url: "https://github.com/vapor/fluent-{{fluent.db.url}}-driver.git", from: "{{fluent.db.version}}-alpha"){{/fluent}}
     ],
     targets: [
         .target(name: "App", dependencies: [{{#fluent}}"Fluent", "Fluent{{fluent.db.module}}Driver", {{/fluent}}"Vapor"]),
