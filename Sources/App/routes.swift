34daf32c9b81ba98750ddf554e10331e5cb928cb
Tanner Nelson <me@tanner.xyz>
2019-10-09 15:28:07 -0400

    add conditions

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,6 +13,5 @@ func routes(_ r: Routes, _ c: Container) throws {
     let todoController = try TodoController(db: c.make())
     r.get("todos", use: todoController.index)
     r.post("todos", use: todoController.create)
-    r.on(.DELETE, "todos", ":todoID", use: todoController.delete)
-    {{/fluent}}
+    r.on(.DELETE, "todos", ":todoID", use: todoController.delete){{/fluent}}
 }
