827200f42cc5c8a4971d6cee053518cd70fd9722
Gwynne Raskind <gwynne@darkrainfall.org>
2020-05-05 14:37:54 -0500

    Add the `-cross-module-optimization` compiler flag when building in Release config. For consistency, use .target(name:) in the `Run` target's dependency.

--- Package.swift
+++ Package.swift
@@ -13,12 +13,21 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-{{fluent.db.url}}-driver.git", from: "{{fluent.db.version}}-rc"){{/fluent}}
     ],
     targets: [
-        .target(name: "App", dependencies: [{{#fluent}}
-            .product(name: "Fluent", package: "fluent"),
-            .product(name: "Fluent{{fluent.db.module}}Driver", package: "fluent-{{fluent.db.url}}-driver"),{{/fluent}}
-            .product(name: "Vapor", package: "vapor")
-        ]),
-        .target(name: "Run", dependencies: ["App"]),
+        .target(
+            name: "App",
+            dependencies: [{{#fluent}}
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "Fluent{{fluent.db.module}}Driver", package: "fluent-{{fluent.db.url}}-driver"),{{/fluent}}
+                .product(name: "Vapor", package: "vapor")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .target(name: "Run", dependencies: [.target(name: "App")]),
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
