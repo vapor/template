a795706c690ad53f63e36a3ba0231327a6aa74ab
grosch <scott.grosch@icloud.com>
2020-11-09 11:18:25 -0800

    Add port to default database setup. (#43)
    * Handles ports by default. Very common for dev's to have a locally running database already
    
    * Update Sources/App/configure.swift
    
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>
    
    * Update Sources/App/configure.swift
    
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>
    
    * Update Sources/App/configure.swift
    
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>
    
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,11 +9,13 @@ public func configure(_ app: Application) throws {
 
     {{#fluent.db.is_postgres}}app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .psql){{/fluent.db.is_postgres}}{{#fluent.db.is_mysql}}app.databases.use(.mysql(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
