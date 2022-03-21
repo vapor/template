af375adb470ec3ffd476c6eb86736dd69e26f31d
De Bock Benny <bdebock@protonmail.com>
2022-03-21 14:42:14 +0100

    remove faults

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -5,6 +5,5 @@ var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
-tis hier nie normaal e die shizzle hier
 try configure(app)
 try app.run()
