68e346c2a98f6075042aed2c65ce8820a677da23
Zsolt Váradi <zsolt@foosio.com>
2022-04-22 19:14:58 +0200

    Start RunLoop.main, run app on BG thread

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -6,4 +6,17 @@ try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
 try configure(app)
-try app.run()
+
+let appThread = Thread {
+    do {
+        try app.run()
+        exit(0)
+    } catch {
+        exit(1)
+    }
+}
+
+appThread.name = "Vapor App"
+appThread.start()
+
+RunLoop.main.run()
