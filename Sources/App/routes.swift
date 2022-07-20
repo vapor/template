c7ef1abd8316ea1b131a8287429861097d480ae1
Galen O’Hanlon <gohanlon@icloud.com>
2022-07-19 17:20:25 -0700

    Update last non-"async/await" routes to preferred "async/await" style
    Resolves #89.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,13 @@
 {{/fluent}}import Vapor
 
 func routes(_ app: Application) throws {
-    {{#leaf}}app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
-    }{{/leaf}}{{^leaf}}app.get { req in
+    {{#leaf}}app.get { req async throws in
+        return try await req.view.render("index", ["title": "Hello Vapor!"])
+    }{{/leaf}}{{^leaf}}app.get { req async in
         return "It works!"
     }{{/leaf}}
 
-    app.get("hello") { req -> String in
+    app.get("hello") { req async -> String in
         return "Hello, world!"
     }{{#fluent}}
 
