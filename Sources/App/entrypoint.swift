24008dabc6b0752c8bbf436a3ce21a414cd3df1d
Zsolt Váradi <zsolt@foosio.com>
2023-02-10 09:53:05 +0100

    Drop Run target and use @main as an entrypoint

---
+++ Sources/App/entrypoint.swift
@@ -0,0 +1,31 @@
+import Vapor
+
+private extension Application {
+    static let runQueue = DispatchQueue(label: "Run")
+
+    func runOnBackgroundThread() async throws {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            Self.runQueue.async { [self] in
+                do {
+                    try run()
+                    continuation.resume()
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
+
+@main
+enum Run {
+    static func main() async throws {
+        var env = try Environment.detect()
+        try LoggingSystem.bootstrap(from: &env)
+        let app = Application(env)
+        defer { app.shutdown() }
+
+        try await configure(app)
+        try await app.runOnBackgroundThread()
+    }
+}
