6568a53ddfe99d05f25590955f7bbac2acbe3832
Gwynne Raskind <gwynne@vapor.codes>
2023-07-18 14:02:30 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,17 @@ import PackageDescription
 let package = Package(
     name: "{{name}}",
     platforms: [
-       .macOS(.v12)
+       .macOS(.v13)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),{{#fluent}}
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.77.1"),{{#fluent}}
+        // 🗄 An ORM for SQL and NoSQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        // {{fluent.db.emoji}} Fluent driver for {{fluent.db.module}}.
         .package(url: "https://github.com/vapor/fluent-{{fluent.db.url}}-driver.git", from: "{{fluent.db.version}}"),{{/fluent}}{{#leaf}}
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),{{/leaf}}
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),{{/leaf}}
     ],
     targets: [
         .executableTarget(
@@ -21,12 +24,6 @@ let package = Package(
                 .product(name: "Fluent{{fluent.db.module}}Driver", package: "fluent-{{fluent.db.url}}-driver"),{{/fluent}}{{#leaf}}
                 .product(name: "Leaf", package: "leaf"),{{/leaf}}
                 .product(name: "Vapor", package: "vapor")
-            ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://www.swift.org/server/guides/building.html#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
