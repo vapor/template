f5bd617910607e07bf1c12afbd055d5e35515e84
Chris Ameter <29186822+ameter@users.noreply.github.com>
2024-03-10 13:11:38 -0600

    fix: support swift strict concurrency

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -11,16 +11,19 @@ struct TodoController: RouteCollection {
         }
     }
 
+    @Sendable
     func index(req: Request) async throws -> [Todo] {
         try await Todo.query(on: req.db).all()
     }
 
+    @Sendable
     func create(req: Request) async throws -> Todo {
         let todo = try req.content.decode(Todo.self)
         try await todo.save(on: req.db)
         return todo
     }
 
+    @Sendable
     func delete(req: Request) async throws -> HTTPStatus {
         guard let todo = try await Todo.find(req.parameters.get("todoID"), on: req.db) else {
             throw Abort(.notFound)
