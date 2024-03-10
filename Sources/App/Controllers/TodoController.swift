d7fc30c9beaac2cc335381d135ea9904b45af109
Gwynne Raskind <gwynne@vapor.codes>
2024-03-10 14:47:44 -0500

    Fix strict concurrency issues

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -4,27 +4,33 @@ import Vapor
 struct TodoController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let todos = routes.grouped("todos")
-        todos.get(use: index)
-        todos.post(use: create)
+
+        todos.get(use: self.index)
+        todos.post(use: self.create)
         todos.group(":todoID") { todo in
-            todo.delete(use: delete)
+            todo.delete(use: self.delete)
         }
     }
-
+    
+    @Sendable
     func index(req: Request) async throws -> [Todo] {
         try await Todo.query(on: req.db).all()
     }
 
+    @Sendable
     func create(req: Request) async throws -> Todo {
         let todo = try req.content.decode(Todo.self)
+
         try await todo.save(on: req.db)
         return todo
     }
 
+    @Sendable
     func delete(req: Request) async throws -> HTTPStatus {
         guard let todo = try await Todo.find(req.parameters.get("todoID"), on: req.db) else {
             throw Abort(.notFound)
         }
+
         try await todo.delete(on: req.db)
         return .noContent
     }
