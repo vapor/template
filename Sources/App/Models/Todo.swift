62fd16d88e9efbf1b8e61fe5b2d0350b4f221f8a
Gwynne Raskind <gwynne@vapor.codes>
2024-04-02 04:06:51 -0500

    Use DTOs for the Todo model.

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -1,10 +1,9 @@
 import Fluent
-import Vapor
 
 /// Property wrappers interact poorly with `Sendable` checking, causing a warning for the `@ID` property
 /// It is recommended you write your model with sendability checking on and then suppress the warning
 /// afterwards with `@unchecked Sendable`.
-final class Todo: Model, Content, @unchecked Sendable {
+final class Todo: Model, @unchecked Sendable {
     static let schema = "todos"
     
     @ID(key: .id)
@@ -18,5 +17,12 @@ final class Todo: Model, Content, @unchecked Sendable {
     init(id: UUID? = nil, title: String) {
         self.id = id
         self.title = title
+    }
+    
+    var dto: TodoDTO {
+        .init(
+            id: self.id,
+            title: self.$title.value
+        )
     }
 }
