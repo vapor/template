e3f4d4a83e8390446550577b0527fc881f4fd448
Gwynne Raskind <gwynne@vapor.codes>
2024-04-02 04:13:46 -0500

    Add Fluent conditionals to tests and add DTOs folder to manifest

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,15 +1,19 @@
 @testable import App
 import XCTVapor
+{{#fluent}}import Fluent
+{{/fluent}}
 
 final class AppTests: XCTestCase {
-    let app: Application!
+    var app: Application!
     
     override func setUp() async throws {
         self.app = Application(.testing)
-        try await configure(app)
+        try await configure(app){{#fluent}}
+        try await app.autoMigrate(){{/fluent}}
     }
     
-    override func tearDown() async throws {
+    override func tearDown() async throws { {{#fluent}}
+        try await app.autoRevert(){{/fluent}}
         self.app.shutdown()
         self.app = nil
     }
@@ -19,7 +23,7 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
-    }
+    }{{#fluent}}
     
     func testTodoIndex() async throws {
         let sampleTodos = [Todo(title: "sample1"), Todo(title: "sample2")]
@@ -28,8 +32,8 @@ final class AppTests: XCTestCase {
         try self.app.test(.GET, "todos", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(
-                try res.content.decode([TodoDTO].self).sorted(by: \.title),
-                sampleTodos.map(\.dto).sorted(by: \.title)
+                try res.content.decode([TodoDTO].self).sorted(by: { $0.title ?? "" < $1.title ?? "" }),
+                sampleTodos.map(\.dto).sorted(by: { $0.title ?? "" < $1.title ?? "" })
             )
         })
     }
@@ -37,12 +41,12 @@ final class AppTests: XCTestCase {
     func testTodoCreate() async throws {
         let newDTO = TodoDTO(id: nil, title: "test")
         
-        try self.app.test(.POST, "todos", beforeRequest: { req in
+        try await self.app.test(.POST, "todos", beforeRequest: { req in
             try req.content.encode(newDTO)
         }, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             let models = try await Todo.query(on: self.app.db).all()
-            XCTAssertEqual(models.map(\.dto), [newDTO])
+            XCTAssertEqual(models.map(\.dto.title), [newDTO.title])
         })
     }
     
@@ -50,10 +54,18 @@ final class AppTests: XCTestCase {
         let testTodos = [Todo(title: "test1"), Todo(title: "test2")]
         try await testTodos.create(on: app.db)
         
-        try self.app.test(.DELETE, "todos/\(testTodos[0].id)", afterResponse: { res in
-            XCTAssertEqual(res.status, .ok)
+        try await self.app.test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res in
+            XCTAssertEqual(res.status, .noContent)
             let model = try await Todo.find(testTodos[0].id, on: self.app.db)
             XCTAssertNil(model)
         })
+    }{{/fluent}}
+}
+{{#fluent}}
+
+extension TodoDTO: Equatable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id && lhs.title == rhs.title
     }
 }
+{{/fluent}}
