3e6e6003f42d1808da27002f936710e8a9f23bf6
Sam Bishop <sambobjr@gmail.com>
2024-04-29 16:21:46 -0600

    Add unchecked sendable to Todo (#130)

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -1,7 +1,10 @@
 import Fluent
 import Vapor
 
-final class Todo: Model, Content {
+/// Property wrappers interact poorly with `Sendable` checking, causing a warning for the `@ID` property
+/// It is recommended you write your model with sendability checking on and then suppress the warning
+/// afterwards with `@unchecked Sendable`.
+final class Todo: Model, Content, @unchecked Sendable {
     static let schema = "todos"
     
     @ID(key: .id)
