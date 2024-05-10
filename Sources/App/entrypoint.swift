7182e23e5488481c1440ae0e4e0fbc9a58cdc822
Tim <0xtimc@gmail.com>
2024-05-10 18:43:49 +0100

    Log executor takeover

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -6,11 +6,15 @@ import NIOPosix
 @main
 enum Entrypoint {
     static func main() async throws {
-        _ = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
         var env = try Environment.detect()
         try LoggingSystem.bootstrap(from: &env)
         
         let app = try await Application.make(env)
+
+        let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
+        if !executorTakeoverSuccess {
+            app.logger.warning("Failed to install global NIO executor, using default")
+        }
         
         do {
             try await configure(app)
