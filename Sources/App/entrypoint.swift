64cc082177b0376f96c70ad9901e083fa75d22f6
Tim Condon <0xTim@users.noreply.github.com>
2024-05-15 18:11:37 +0200

    Async Entry and Concurrency Executor Takeover (#131)

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -1,5 +1,7 @@
 import Vapor
 import Logging
+import NIOCore
+import NIOPosix
 
 @main
 enum Entrypoint {
@@ -7,15 +9,21 @@ enum Entrypoint {
         var env = try Environment.detect()
         try LoggingSystem.bootstrap(from: &env)
         
-        let app = Application(env)
-        defer { app.shutdown() }
+        let app = try await Application.make(env)
+
+        // This attempts to install NIO as the Swift Concurrency global executor.
+        // You should not call any async functions before this point.
+        let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
+        app.logger.debug("Running with \(executorTakeoverSuccess ? "SwiftNIO" : "standard") Swift Concurrency default executor")
         
         do {
             try await configure(app)
         } catch {
             app.logger.report(error: error)
+            try? await app.asyncShutdown()
             throw error
         }
         try await app.execute()
+        try await app.asyncShutdown()
     }
 }
