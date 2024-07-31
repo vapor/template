62d8d4dbb0c290f1b7aab8053d8b15c1c7f68249
Tim <0xtimc@gmail.com>
2024-07-31 14:26:43 +0100

    Disable by default and add warning

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -12,9 +12,11 @@ enum Entrypoint {
         let app = try await Application.make(env)
 
         // This attempts to install NIO as the Swift Concurrency global executor.
-        // You should not call any async functions before this point.
-        let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
-        app.logger.debug("Running with \(executorTakeoverSuccess ? "SwiftNIO" : "standard") Swift Concurrency default executor")
+        // You can enable it if you'd like to reduce the amount of context switching between NIO and Swift Concurrency.
+        // Note: this has caused issues with some libraries that use `.wait()` and cleanly shutting down.
+        // If enabled, you should be careful about calling async functions before this point as it can cause assertion failures.
+        // let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
+        // app.logger.debug("Running with \(executorTakeoverSuccess ? "SwiftNIO" : "standard") Swift Concurrency default executor")
         
         do {
             try await configure(app)
