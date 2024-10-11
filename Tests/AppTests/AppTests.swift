c7402ea93924f6baad5d6057e5709c018ae39ad6
Tim <0xtimc@gmail.com>
2024-10-11 12:36:04 +0100

    Don't add migrations with no Fluent

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -11,10 +11,10 @@ struct AppTests {
     private func withApp(_ test: (Application) async throws -> ()) async throws {
         let app = try await Application.make(.testing)
         try await configure(app)
-        try await app.autoMigrate()
-        try await test(app)
-        try await app.autoRevert()
-        try await app.asyncShutdown()
+        {{#fluent}}try await app.autoMigrate()   
+        {{/fluent}}try await test(app)
+        {{#fluent}}try await app.autoRevert()   
+        {{/fluent}}try await app.asyncShutdown()
     }
     
     @Test("Test Hello World Route")
