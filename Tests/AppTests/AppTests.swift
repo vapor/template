fe674941350fb51d4c27ea178308de3e61ba0c0e
Tim <0xtimc@gmail.com>
2024-10-11 12:33:04 +0100

    Migrate tests to Swift Testing

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,64 +1,74 @@
 @testable import App
 import XCTVapor
+import Testing
 {{#fluent}}import Fluent
 {{/fluent}}
 
-final class AppTests: XCTestCase {
-    var app: Application!
-    
-    override func setUp() async throws {
-        self.app = try await Application.make(.testing)
-        try await configure(app){{#fluent}}
-        try await app.autoMigrate(){{/fluent}}
+{{#fluent}}@Suite("App Tests with DB", .serialized)
+{{/fluent}}{{^fluent}}@Suite("App Tests")
+{{/fluent}}
+struct AppTests {
+    private func withApp(_ test: (Application) async throws -> ()) async throws {
+        let app = try await Application.make(.testing)
+        try await configure(app)
+        try await app.autoMigrate()
+        try await test(app)
+        try await app.autoRevert()
+        try await app.asyncShutdown()
     }
     
-    override func tearDown() async throws { {{#fluent}}
-        try await app.autoRevert(){{/fluent}}
-        try await self.app.asyncShutdown()
-        self.app = nil
-    }
-    
-    func testHelloWorld() async throws {
-        try await self.app.test(.GET, "hello", afterResponse: { res async in
-            XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
-        })
+    @Test("Test Hello World Route")
+    func helloWorld() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "hello", afterResponse: { res async in
+                #expect(res.status == .ok)
+                #expect(res.body.string == "Hello, world!")
+            })
+        }
     }{{#fluent}}
     
-    func testTodoIndex() async throws {
-        let sampleTodos = [Todo(title: "sample1"), Todo(title: "sample2")]
-        try await sampleTodos.create(on: self.app.db)
-        
-        try await self.app.test(.GET, "todos", afterResponse: { res async throws in
-            XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(
-                try res.content.decode([TodoDTO].self).sorted(by: { $0.title ?? "" < $1.title ?? "" }),
-                sampleTodos.map { $0.toDTO() }.sorted(by: { $0.title ?? "" < $1.title ?? "" })
-            )
-        })
+    @Test("Getting all the Todos")
+    func getAllTodos() async throws {
+        try await withApp { app in
+            let sampleTodos = [Todo(title: "sample1"), Todo(title: "sample2")]
+            try await sampleTodos.create(on: app.db)
+            
+            try await app.test(.GET, "todos", afterResponse: { res async throws in
+                #expect(res.status == .ok)
+                #expect(try res.content.decode([TodoDTO].self) == sampleTodos.map { $0.toDTO()} )
+            })
+        }
     }
     
-    func testTodoCreate() async throws {
+    @Test("Creating a Todo")
+    func createTodo() async throws {
         let newDTO = TodoDTO(id: nil, title: "test")
         
-        try await self.app.test(.POST, "todos", beforeRequest: { req in
-            try req.content.encode(newDTO)
-        }, afterResponse: { res async throws in
-            XCTAssertEqual(res.status, .ok)
-            let models = try await Todo.query(on: self.app.db).all()
-            XCTAssertEqual(models.map { $0.toDTO().title }, [newDTO.title])
-        })
+        try await withApp { app in
+            try await app.test(.POST, "todos", beforeRequest: { req in
+                try req.content.encode(newDTO)
+            }, afterResponse: { res async throws in
+                #expect(res.status == .ok)
+                let models = try await Todo.query(on: app.db).all()
+                #expect(models.map({ $0.toDTO().title }) == [newDTO.title])
+                XCTAssertEqual(models.map { $0.toDTO() }, [newDTO])
+            })
+        }
     }
     
-    func testTodoDelete() async throws {
+    @Test("Deleting a Todo")
+    func deleteTodo() async throws {
         let testTodos = [Todo(title: "test1"), Todo(title: "test2")]
-        try await testTodos.create(on: app.db)
         
-        try await self.app.test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res async throws in
-            XCTAssertEqual(res.status, .noContent)
-            let model = try await Todo.find(testTodos[0].id, on: self.app.db)
-            XCTAssertNil(model)
-        })
+        try await withApp { app in
+            try await testTodos.create(on: app.db)
+            
+            try await app.test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res async throws in
+                #expect(res.status == .noContent)
+                let model = try await Todo.find(testTodos[0].id, on: app.db)
+                #expect(model == nil)
+            })
+        }
     }{{/fluent}}
 }
 {{#fluent}}
