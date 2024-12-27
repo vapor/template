8c30547a51ef36b569634b86199f5b0b883c3ca4
MahdiBM <github@mahdibm.com>
2024-12-27 18:20:20 +0330

    Use VaporTesting

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,5 +1,5 @@
 @testable import App
-import XCTVapor
+import VaporTesting
 import Testing
 {{#fluent}}import Fluent
 {{/fluent}}
@@ -27,7 +27,7 @@ struct AppTests {
     @Test("Test Hello World Route")
     func helloWorld() async throws {
         try await withApp { app in
-            try await app.test(.GET, "hello", afterResponse: { res async in
+            try await app.testing().test(.GET, "hello", afterResponse: { res async in
                 #expect(res.status == .ok)
                 #expect(res.body.string == "Hello, world!")
             })
@@ -40,7 +40,7 @@ struct AppTests {
             let sampleTodos = [Todo(title: "sample1"), Todo(title: "sample2")]
             try await sampleTodos.create(on: app.db)
             
-            try await app.test(.GET, "todos", afterResponse: { res async throws in
+            try await app.testing().test(.GET, "todos", afterResponse: { res async throws in
                 #expect(res.status == .ok)
                 #expect(try res.content.decode([TodoDTO].self) == sampleTodos.map { $0.toDTO()} )
             })
@@ -52,13 +52,12 @@ struct AppTests {
         let newDTO = TodoDTO(id: nil, title: "test")
         
         try await withApp { app in
-            try await app.test(.POST, "todos", beforeRequest: { req in
+            try await app.testing().test(.POST, "todos", beforeRequest: { req in
                 try req.content.encode(newDTO)
             }, afterResponse: { res async throws in
                 #expect(res.status == .ok)
                 let models = try await Todo.query(on: app.db).all()
                 #expect(models.map({ $0.toDTO().title }) == [newDTO.title])
-                XCTAssertEqual(models.map { $0.toDTO() }, [newDTO])
             })
         }
     }
@@ -70,7 +69,7 @@ struct AppTests {
         try await withApp { app in
             try await testTodos.create(on: app.db)
             
-            try await app.test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res async throws in
+            try await app.testing().test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res async throws in
                 #expect(res.status == .noContent)
                 let model = try await Todo.find(testTodos[0].id, on: app.db)
                 #expect(model == nil)
