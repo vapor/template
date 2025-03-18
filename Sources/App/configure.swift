7605eb34910264dd69d11be6e2d6ad61fff57029
Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>
2025-03-18 17:37:21 +0100

    Update template for new Toolbox (#159)
    * Update for new Toolbox rendering
    
    * Try fixing AppTests
    
    * Try fixing configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,19 +16,17 @@ public func configure(_ app: Application) async throws {
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database",
         tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql){{/fluent.db.is_postgres}}{{#fluent.db.is_mysql}}    app.databases.use(DatabaseConfigurationFactory.mysql(
+    ), as: .psql){{/fluent.db.is_postgres}}{{#fluent.db.is_mysql}}app.databases.use(DatabaseConfigurationFactory.mysql(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .mysql){{/fluent.db.is_mysql}}{{#fluent.db.is_sqlite}}    app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite){{/fluent.db.is_sqlite}}
+    ), as: .mysql){{/fluent.db.is_mysql}}{{#fluent.db.is_sqlite}}app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite){{/fluent.db.is_sqlite}}
 
     app.migrations.add(CreateTodo()){{/fluent}}{{#leaf}}
 
-    app.views.use(.leaf)
-
-    {{/leaf}}
+    app.views.use(.leaf){{/leaf}}
 
     // register routes
     try routes(app)
