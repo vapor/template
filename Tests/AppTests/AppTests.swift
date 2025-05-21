1bfdbcc707831711c3d6109d1fd732a075a53df1
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-05-21 16:49:22 +0200

    Revert "Use VaporTesting's `withApp` even when using Fluent"
    This reverts commit b504efaa2c738e7e2fcff8b196b0304a7d1ee1f5.

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -9,16 +9,18 @@ import Testing
 {{/fluent}}
 struct {{name}}Tests {
     {{#fluent}}private func withApp(_ test: (Application) async throws -> ()) async throws {
-        try await VaporTesting.withApp(configure: configure) { app in
-            do {
-                try await app.autoMigrate()
-                try await test(app)
-                try await app.autoRevert()
-            } catch {
-                try? await app.autoRevert()
-                throw error
-            }
+        let app = try await Application.make(.testing)
+        do {
+            try await configure(app)
+            try await app.autoMigrate()
+            try await test(app)
+            try await app.autoRevert()
+        } catch {
+            try? await app.autoRevert()
+            try await app.asyncShutdown()
+            throw error
         }
+        try await app.asyncShutdown()
     }
     
     {{/fluent}}@Test("Test Hello World Route")
